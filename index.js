d1c4e3e99f1387099f30d7b62072195bce3f3ac6
Edmundo Alvarez <edmundoalvarezok@gmail.com>
2024-01-14 14:56:52 -0300

    index.js & vercel.json

--- index.js
+++ index.js
@@ -13,20 +13,6 @@ app.use('/api', AccountRoute)
 
 const port = process.env.PORT || 2023;
 
-app.get("/", (req, res) => {
-    const htmlResponse = `
-    <html>
-        <head>
-            <title>Api Votación de Juegos</title>
-        </head>
-        <body>
-            <h1>Accediste a la api de votación de juegos</h1>
-        </body>
-    </html>
-    `;
-    res.send(htmlResponse);
-});
-
  app.listen(port, () => {
     console.log(`Port running in: http://localhost:${port}`);
  });
