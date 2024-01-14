0e2416f0e718057b8b9c7b23fda158f43298dfd5
Edmundo Alvarez <edmundoalvarezok@gmail.com>
2024-01-14 14:31:34 -0300

    update index.js

--- index.js
+++ index.js
@@ -8,9 +8,26 @@ app.use(express.json())
 app.use(cors())
 
 app.use(GamesRoute)
-
 app.use('/api', AccountRoute)
 
-app.listen(2023, function (){
-    console.log("El servidor está levantado! http://localhost:2023")
-  })
+
+const port = process.env.PORT || 2023;
+
+app.get("/", (req, res) => {
+    const htmlResponse = `
+    <html>
+        <head>
+            <title>Api Votación de Juegos</title>
+        </head>
+        <body>
+            <h1>Accediste a la api de votación de juegos</h1>
+        </body>
+    </html>
+    `;
+    res.send(htmlResponse);
+});
+
+ app.listen(port, () => {
+    console.log(`Port running in: http://localhost:${port}`);
+ });
+ 
